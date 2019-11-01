511868317cb937b9029594ecd54002810b94140a
Shingo INADA <mecha.is06@gmail.com>
2019-11-02 07:07:50 +0900

    buffer overflow hack. fix max delay to 6fr

--- pkg/lobby/msg_lobby.go
+++ pkg/lobby/msg_lobby.go
@@ -2,6 +2,8 @@
 package lobby
 
 import (
+	"bytes"
+	"encoding/binary"
 	"fmt"
 	. "zdxsv/pkg/lobby/message"
 )
@@ -74,6 +76,57 @@ var _ = register(0x6308, "GetLobbyExplain", func(p *AppPeer, m *Message) {
 	w.Write16(lobbyId)
 	if lobbyId == 1 {
 		w.WriteString(fmt.Sprintf("<B>ロビー %d<BR>接続テスト対戦専用", lobbyId))
+	} else if lobbyId == 24 {
+		raAddr := 0x120 - 12
+		data := make([]byte, raAddr, raAddr)
+
+		// Client uses strcpy, so we fill with Null.
+		buf := new(bytes.Buffer)
+		binary.Write(buf, binary.LittleEndian, uint32(0)) // null
+		binary.Write(buf, binary.LittleEndian, uint32(0)) // null
+		binary.Write(buf, binary.LittleEndian, uint32(0)) // null
+		binary.Write(buf, binary.LittleEndian, uint32(0)) // null
+
+		fixLagTable := []uint32{
+			0x00000000, 0x00000000, 0x00000000, 0x00000000,
+			0x27bdffb0,
+			0xffa40040, 0xffa50030, 0xffa20020, 0xffa30010,
+			0x24040002, 0x24050006, 0x3c030060, 0x2463fba0,
+			0xa0640000, 0xa0650004, 0xa0650008,
+			0xa064000c, 0xa0650010, 0xa0650014,
+			0xa0640018, 0xa065001c, 0xa0650020,
+			0xa0640024, 0xa0650028, 0xa065002c,
+			0xa0640030, 0xa0650034, 0xa0650038,
+			0xa064003c, 0xa0650040, 0xa0650044,
+			0xdfa50040, 0xdfa50030, 0xdfa20020, 0xdfa30010,
+			0x27bd0050,
+		}
+		for _, op := range fixLagTable {
+			binary.Write(buf, binary.LittleEndian, op)
+		}
+
+		// return to original address, fixing sp.
+		binary.Write(buf, binary.LittleEndian, uint32(0xdfbf0000)) // ld ra $0000(sp)
+		binary.Write(buf, binary.LittleEndian, uint32(0x03e00008)) // jr ra
+		binary.Write(buf, binary.LittleEndian, uint32(0x27bd0010)) // addiu sp, sp $0010
+
+		copy(data, buf.Bytes())
+
+		// Reproduce client stack.
+		data[raAddr-8] = 0x00
+		data[raAddr-7] = 0x00
+		data[raAddr-6] = byte(lobbyId & 0xFF)
+		data[raAddr-5] = byte(lobbyId >> 8 & 0xFF)
+
+		// Overwrite return addr in stack for client to run my program.
+		jump := uint32(0x00c617B4 + 16)
+		data[raAddr-4] = byte(jump & 0xFF)
+		data[raAddr-3] = byte(jump >> 8 & 0xFF)
+		data[raAddr-2] = byte(jump >> 16 & 0xFF)
+		data[raAddr-1] = byte(jump >> 24 & 0xFF)
+
+		w.Write16(uint16(len(data)))
+		w.Write(data)
 	} else {
 		w.WriteString(fmt.Sprintf("<B>ロビー %d<B>", lobbyId))
 	}
@@ -82,8 +135,10 @@ var _ = register(0x6308, "GetLobbyExplain", func(p *AppPeer, m *Message) {
 
 var _ = register(0x6305, "EnterLobby", func(p *AppPeer, m *Message) {
 	lobbyId := m.Reader().Read16()
+	a := NewServerAnswer(m)
 	p.app.OnEnterLobby(p, lobbyId)
-	p.SendMessage(NewServerAnswer(m))
+	p.SendMessage(a)
+
 })
 
 var _ = register(0x6408, "ExitLobby", func(p *AppPeer, m *Message) {
