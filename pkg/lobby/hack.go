cb85f9d8ca5bf94227ac9dcb92775f8f34a7eaf7
Shingo INADA <mecha.is06@gmail.com>
2019-11-03 21:43:06 +0900

    add widescreen patch

--- pkg/lobby/hack.go
+++ pkg/lobby/hack.go
@@ -1,27 +1,41 @@
 package lobby
 
 import (
-	"fmt"
-
+	"zdxsv/pkg/db"
 	. "zdxsv/pkg/lobby/message"
-
-	"github.com/golang/glog"
 )
+
+func SendLobbyChatHackNotice(p *AppPeer) {
+	NoticeChatMessage(p, "SERVER", "＞", "自動選抜に入ることでＵＤＰプロキシの")
+	NoticeChatMessage(p, "SERVER", "＞", "接続テスト対戦を行うことができます。")
+	NoticeChatMessage(p, "SERVER", "＞", "利用可能コマンド")
+	NoticeChatMessage(p, "SERVER", "＞", "ｗ０：ワイドスクリーンパッチ無効化")
+	NoticeChatMessage(p, "SERVER", "＞", "ｗ１：ワイドスクリーンパッチ有効化")
+}
+
+func LobbyChatHack(p *AppPeer, str string) {
+	switch str {
+	case "ｗ０":
+		p.System = (p.System & uint32(0xffffff00)) | 0x00
+		NoticeChatMessage(p, "SERVER", "＞", "ワイドスクリーンパッチを無効化")
+		NoticeChatMessage(p, "SERVER", "＞", "戦場選択画面に戻ってください")
+		NoticeChatMessage(p, "SERVER", "＞", "この設定はユーザ別に保存されます")
+		db.DefaultDB.UpdateUser(&p.User.User)
+	case "ｗ１":
+		p.System = (p.System & uint32(0xffffff00)) | 0x01
+		NoticeChatMessage(p, "SERVER", "＞", "ワイドスクリーンパッチを有効化")
+		NoticeChatMessage(p, "SERVER", "＞", "戦場選択画面に戻ってください")
+		NoticeChatMessage(p, "SERVER", "＞", "この設定はユーザ別に保存されます")
+		db.DefaultDB.UpdateUser(&p.User.User)
+	}
+}
 
 // SetPadDelayLobbyHack writes answer of LobbyExplain message
 // with a function that sets pad delay to static value.
-// NOTE: This function only works when lobby_id = 3.
 func SetPadDelayLobbyHack(p *AppPeer, m *Message) *Message {
 	lobbyId := m.Reader().Read16()
 	a := NewServerAnswer(m)
 	w := a.Writer()
-
-	if lobbyId != uint16(3) {
-		glog.Warningln("SetPadDelay Failed lobbyId must be 3")
-		w.Write16(lobbyId)
-		w.WriteString(fmt.Sprintf("<B>Lobby %d<B>", lobbyId))
-		return a
-	}
 
 	// FIX DELAY TABLE HACK
 
@@ -48,7 +62,7 @@ func SetPadDelayLobbyHack(p *AppPeer, m *Message) *Message {
 
 	// R5900 Function: Fill pad delay table
 	// (initial, soft_limit, hard_limit) * 6 to static value.
-	fixLagTable := []uint32{
+	for _, op := range []uint32{
 		0x27bdffb0, // sp -= 0x0050
 
 		0xffa40040, 0xffa50030, 0xffa20020, 0xffa30010, // save a0, a1, v0, v1 to stack
@@ -64,9 +78,87 @@ func SetPadDelayLobbyHack(p *AppPeer, m *Message) *Message {
 		0xdfa40040, 0xdfa50030, 0xdfa20020, 0xdfa30010, // load a0, a1, v0, v1 from stack
 
 		0x27bd0050, // sp += 0x0050
+	} {
+		w.Write32LE(op)
 	}
 
-	for _, op := range fixLagTable {
+	// return to original address, fixing sp.
+	w.Write32LE(uint32(0xdfbf0000)) // ld ra $0000(sp)
+	w.Write32LE(uint32(0x03e00008)) // jr ra
+	w.Write32LE(uint32(0x27bd0010)) // addiu sp, sp $0010
+
+	// padding
+	for w.BodyLen() < targetBodySize-8 {
+		w.Write8(uint8(0))
+	}
+
+	// Reproduce client stack.
+	w.Write16LE(0)
+	w.Write16LE(lobbyId)
+
+	// Overwrite return addr in stack for client to run the function.
+	w.Write32LE(uint32(0x00c22cc0))
+
+	return a
+}
+
+// SetWideScreenLobbyHack writes answer of LobbyExplain message
+// with a function that sets widescreen patch.
+func SetWideScreenLobbyHack(p *AppPeer, m *Message) *Message {
+	lobbyId := m.Reader().Read16()
+	a := NewServerAnswer(m)
+	w := a.Writer()
+
+	targetBodySize := 0x0120 - 8
+
+	w.Write16(lobbyId)
+	w.Write16(uint16(targetBodySize - 4))
+	w.Write8('<')
+	w.Write8('B')
+	w.Write8('>')
+	w.Write8('w')
+	w.Write8('i')
+	w.Write8('d')
+	w.Write8('e')
+	w.Write8('s')
+	w.Write8('c')
+	w.Write8('r')
+	w.Write8('e')
+	w.Write8('e')
+	w.Write8('n')
+	w.Write8(0)
+	w.Write8(0)
+	w.Write8(0)
+	w.Write32(uint32(0))
+	w.Write32(uint32(0))
+	w.Write32(uint32(0))
+
+	fixvalue01 := uint32(0x24843f40)
+	fixvalue02 := uint32(0x3c044440)
+	fixvalue03 := uint32(0x2485e7ff)
+
+	if p.System&0x0f == 0 {
+		// set original value to disable the patch
+		fixvalue01 = uint32(0x24843f80)
+		fixvalue02 = uint32(0x3c044480)
+		fixvalue03 = uint32(0x2485e000)
+	}
+
+	// apply wide screen patch.
+	for _, op := range []uint32{
+		0x27bdffb0, // sp -= 0x0050
+
+		0xffa40040, 0xffa50030, 0xffa20020, 0xffa30010, // save a0, a1, v0, v1 to stack
+
+		// wide screen patch
+		0x3c020027, 0x3c043c02, fixvalue01, 0xac44cf84,
+		0x3c020084, 0x3c0444c0, 0x2485f400, 0xac453d30,
+		fixvalue02, fixvalue03, 0xac453ef0, 0xac4540b0,
+
+		0xdfa40040, 0xdfa50030, 0xdfa20020, 0xdfa30010, // load a0, a1, v0, v1 from stack
+
+		0x27bd0050, // sp += 0x0050
+	} {
 		w.Write32LE(op)
 	}
 
