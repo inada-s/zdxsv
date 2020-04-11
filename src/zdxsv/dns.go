2cbbf414853479a5a1874b6aff8b06c332f39bcb
Shingo INADA <mecha.is06@gmail.com>
2020-04-11 23:00:02 +0900

    add gdxsv dns

--- src/zdxsv/dns.go
+++ src/zdxsv/dns.go
@@ -12,6 +12,7 @@ import (
 var (
 	dnasip    = flag.String("dnasip", "", "Public IP Addr of DNAS server")
 	loginsvip = flag.String("loginsvip", "", "Public Addr of Login server")
+	gdxsvip   = flag.String("gdxsvip", "", "Public IP Addr of GundamDX server")
 )
 
 func makeDNSHandler(record string) func(dns.ResponseWriter, *dns.Msg) {
@@ -41,10 +42,18 @@ func mainDNS() {
 		loginPublicIP = config.Conf.Login.PublicAddr
 	}
 
+	gdxsvPublicIP := *gdxsvip
+	if *gdxsvip == "" {
+		gdxsvPublicIP = config.Conf.Login.PublicAddr
+	}
+
 	log.Println("DNAS server ", dnassvIP)
 	log.Println("Login server ", loginPublicIP)
+	log.Println("gdx server", gdxsvPublicIP)
 	dns.HandleFunc("kddi-mmbb.jp", makeDNSHandler("www01.kddi-mmbb.jp. 3600 IN A "+loginPublicIP))
 	dns.HandleFunc("playstation.org", makeDNSHandler("gate1.jp.dnas.playstation.org. 3600 IN A "+dnassvIP))
+	dns.HandleFunc("ca1203.mmcp6", makeDNSHandler("ca1203.mmcp6. 3600 IN A "+gdxsvPublicIP))
+	dns.HandleFunc("ca1202.mmcp6", makeDNSHandler("ca1202.mmcp6. 3600 IN A "+gdxsvPublicIP))
 
 	server := &dns.Server{Addr: ":53", Net: "udp"}
 	err := server.ListenAndServe()
