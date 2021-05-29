d5f11eb5ec0ac89dfdb57053cc9ca32ee42cfd77
Shingo INADA <mecha.is06@gmail.com>
2021-05-29 15:58:34 +0900

    update goupnp and zproxy to support many routers

--- src/zproxy/upnp.go
+++ src/zproxy/upnp.go
@@ -1,71 +1,88 @@
 package main
 
 import (
+	"context"
+	"github.com/huin/goupnp"
+	"golang.org/x/sync/errgroup"
 	"log"
 
 	"github.com/huin/goupnp/dcps/internetgateway2"
 )
 
-func addUDPPortMapping(localIP string, port uint16) {
-	clients, errors, err := internetgateway2.NewWANPPPConnection1Clients()
-	if err != nil {
-		log.Println(err)
-		return
-	}
+type RouterClient interface {
+	AddPortMapping(
+		NewRemoteHost string,
+		NewExternalPort uint16,
+		NewProtocol string,
+		NewInternalPort uint16,
+		NewInternalClient string,
+		NewEnabled bool,
+		NewPortMappingDescription string,
+		NewLeaseDuration uint32,
+	) (err error)
 
-	log.Printf("Got %d errors finding servers and %d successfully discovered.\n",
-		len(errors), len(clients))
+	GetExternalIPAddress() (
+		NewExternalIPAddress string,
+		err error,
+	)
 
-	for i, e := range errors {
-		log.Printf("Error finding server #%d: %v\n", i+1, e)
-	}
-
-	for _, c := range clients {
-		dev := &c.ServiceClient.RootDevice.Device
-		srv := c.ServiceClient.Service
-		scpd, err := srv.RequestSCDP()
-		if err != nil {
-			log.Println(err)
-			continue
-		}
-		log.Println(dev.FriendlyName, " :: ", srv.String())
-		if scpd == nil || scpd.GetAction("AddPortMapping") != nil {
-			err := c.AddPortMapping("", port, "UDP", port, localIP, true, "zdxsv udp proxy port mapping.", 3600 * 24)
-			if err != nil {
-				log.Println("addUDPPortMapping error: ", err)
-			} else {
-				log.Println("UPnPによりポート開放しました", dev.ModelName, localIP, port)
-			}
-		}
-	}
+	GetServiceClient() *goupnp.ServiceClient
 }
 
-func delUDPPortMapping(localIP string, port uint16) {
-	clients, errors, err := internetgateway2.NewWANPPPConnection1Clients()
+func pickRouterClients() ([]RouterClient, error) {
+	tasks, _ := errgroup.WithContext(context.Background())
+	var clients []RouterClient
+
+	tasks.Go(func() error {
+		cs, _, err := internetgateway2.NewWANIPConnection1Clients()
+		for _, c := range cs {
+			clients = append(clients, c)
+		}
+		return err
+	})
+
+	tasks.Go(func() error {
+		cs, _, err := internetgateway2.NewWANIPConnection2Clients()
+		for _, c := range cs {
+			clients = append(clients, c)
+		}
+		return err
+	})
+
+	tasks.Go(func() error {
+		cs, _, err := internetgateway2.NewWANPPPConnection1Clients()
+		for _, c := range cs {
+			clients = append(clients, c)
+		}
+		return err
+	})
+
+	if err := tasks.Wait(); err != nil {
+		return nil, err
+	}
+
+	return clients, nil
+}
+
+func addUDPPortMapping(localIP string, port uint16) {
+	clients, err := pickRouterClients()
 	if err != nil {
 		log.Println(err)
 		return
 	}
-
-	log.Printf("Got %d errors finding servers and %d successfully discovered.\n",
-		len(errors), len(clients))
-
-	for i, e := range errors {
-		log.Printf("Error finding server #%d: %v\n", i+1, e)
-	}
+	log.Printf("%v Router(s) found.", len(clients))
 
 	for _, c := range clients {
-		dev := &c.ServiceClient.RootDevice.Device
-		srv := c.ServiceClient.Service
-		scpd, err := srv.RequestSCDP()
+		err = c.AddPortMapping( "", port, "UDP", port, localIP, true, "zdxsv udp proxy port mapping.", 3600*24)
 		if err != nil {
-			log.Println(err)
-			continue
-		}
-		log.Println(dev.FriendlyName, " :: ", srv.String())
-		if scpd == nil || scpd.GetAction("DeletePortMapping") != nil {
-			err := c.DeletePortMapping("", port, "UDP")
-			log.Println("delUDPPortMapping: ", err)
+			log.Println("addUDPPortMapping error: ", err)
+		} else {
+			model := ""
+			if c.GetServiceClient() != nil && c.GetServiceClient().RootDevice != nil {
+				model = c.GetServiceClient().RootDevice.Device.ModelName
+			}
+			log.Println("UPnPによりポート開放しました", model, localIP, port)
 		}
 	}
+
 }
